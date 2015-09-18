0eb80c91e3422e90d7ceffaebdf1228adcafdf58
Jeremy Bowers <jeremyjbowers@gmail.com>
2015-09-17 20:39:33 -0400

    Bump version.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def read(filename):
 
 setup(
     name='nfl-stats',
-    version='0.0.3',
+    version='0.0.4',
     author='Jeremy Bowers',
     author_email='jeremyjbowers@gmail.com',
     url='https://github.com/jeremyjbowers/nfl-stats',
